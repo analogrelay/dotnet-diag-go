bc941c1b5115231eceb4663297f98b722f458d7b
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-10-11 12:05:54 -0700

    initial stuff

---
+++ sample-app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            Console.CancelKeyPress += (sender, a) => {
+                a.Cancel = true;
+                Console.WriteLine("Stopping...");
+                tcs.TrySetResult(null);
+            };
+            Console.WriteLine("Running. Press Ctrl-C to stop.");
+            await tcs.Task;
+        }
+    }
+}
